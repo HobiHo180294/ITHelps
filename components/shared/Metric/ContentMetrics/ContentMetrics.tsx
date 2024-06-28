376b130c52d0e5a0a65061614e59539464ab868c
Yehor Potebenko <hitechnic.uu68@gmail.com>
2024-06-28 16:18:12 +0200

    refactor: remove unnecessary comment

--- components/shared/Metric/ContentMetrics/ContentMetrics.tsx
+++ components/shared/Metric/ContentMetrics/ContentMetrics.tsx
@@ -1,11 +1,9 @@
-// Імпортування необхідних модулей
 import Image from 'next/image';
 import { MetricProps } from '../Metric.interface';
 import { contentMetricsStyled } from './ContentMetrics.styles';
 
 export const ContentMetrics = (props: MetricProps): React.JSX.Element => (
 	<>
-		{/* Компонент Image для відображення зображення */}
 		<Image
 			src={props.imgUrl}
 			width={16}
@@ -15,12 +13,10 @@ export const ContentMetrics = (props: MetricProps): React.JSX.Element => (
 		/>
 
 		<p className={`${props.textStyles} ${contentMetricsStyled.paragraph}`}>
-			{/* Значення тексту, передане від батьківського компоненту */}
 			{props.value}
 			<span
 				className={`${contentMetricsStyled.paragraphSpan} ${props.isAuthor ? contentMetricsStyled.paragraphSpanIsAuthor : ''}`}
 			>
-				{/* Заголовок, переданий від батьківського компоненту */}
 				{props.title}
 			</span>
 		</p>
